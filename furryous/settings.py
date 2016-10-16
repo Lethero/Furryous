f799c949ce2073be864a756481c6352e1009b8dd
Ashton Night <Ashton Night>
2016-10-15 23:46:27 -0500

    Modified settings

--- furryous/settings.py
+++ furryous/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'furryous.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'furryous.db'),
     }
 }
 
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "media/"
